e0db2a833acd1497b6aab5fc42cdf21054c5f25e
MatthiasLEBK <123643251+MatthiasLEBK@users.noreply.github.com>
2023-02-02 09:20:33 +0000

    testing canvas

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 placeteam = {};
 
-placeteam.canvas = document.getElementById("pixelcanvas");
-placeteam.setPixel = (x,y,color) => {
+placeteam.ctx = document.getElementById("pixelcanvas").getContext("2d");
+placeteam.init = (pixelmap) => {
+    pixelmap.forEach((lines,y) => {
+        lines.forEach((pixelcolor,x) => {
+            placeteam.setPixel(x,y,pixelcolor);
+        });
+    });
 
+};
+placeteam.setPixel = (x,y,color) => {    
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, 1, 1);
 };
---
+++ scripts/test.js
@@ -0,0 +1,10 @@
+var arr = new Array(200), i, l;
+for(i = 0, l = 200; i < l; i++) {
+    arr[i] = new Array(200);
+}
+arr.forEach((lines,y) => {
+    lines.forEach((pixelcolor,x) => {
+        pixelcolor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    });
+});
+placeteam.init(arr);
