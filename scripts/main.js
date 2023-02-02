81c0e80208b26230021e7cc7600f3316e93f35d9
MatthiasLEBK <123643251+MatthiasLEBK@users.noreply.github.com>
2023-02-02 09:58:52 +0000

    websocket activation

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,19 @@ window.addEventListener("load", (event) => {
 
     placeteam.ctx = document.getElementById("pixelcanvas").getContext("2d");
     placeteam.init = (pixelmap) => {
-        pixelmap.forEach((lines,y) => {
-            lines.forEach((pixelcolor,x) => {
-                placeteam.setPixel(x,y,pixelcolor);
-            });
-        });
 
+
+        // pixelmap.forEach((lines,y) => {
+        //     lines.forEach((pixelcolor,x) => {
+        //         placeteam.setPixel(x,y,pixelcolor);
+        //     });
+        // });
     };
+    placeteamm.update((updatedata) => {
+        updatedata.pixels.forEach((pixels) => {
+            placeteam.setPixel(pixel.position.x,pixel.position.y,pixel.color)
+        });
+    });
     placeteam.setPixel = (x,y,color) => {    
         placeteam.ctx.fillStyle = color;
         placeteam.ctx.fillRect(x, y, 1, 1);
