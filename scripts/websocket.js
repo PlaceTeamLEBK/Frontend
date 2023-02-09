7781e604bdd713665b279d8317ae27496e120a8c
MatthiasLEBK <123643251+MatthiasLEBK@users.noreply.github.com>
2023-02-09 09:41:36 +0000

    push

--- scripts/websocket.js
+++ scripts/websocket.js
@@ -1,32 +1,37 @@
-// window.addEventListener("load", (event) => {
-//     placeteam.websocket = new WebSocket('ws://'+window.location.host+'/websocket, protocols)');
+window.addEventListener("load", (event) => {
+    placeteam.websocket = new WebSocket('ws://'+window.location.host+'/websocket, protocols)');
 
-//     //open websocket and receive Data
-//     placeteam.websocket.onopen = function(e) {
-//         console.log("[open] Connection established");
-//         console.log("Sending to server");
-//         socket.send("My name is John");
-//     };
+    //open websocket and receive Data
+    placeteam.websocket.onopen = function(e) {
+        console.log("[open] Connection established");
+        console.log("Sending to server");
+        // socket.send("My name is John");
+    };
 
-//     //on update from server
-//     placeteam.websocket.onmessage = function(event) {
-//         console.log(`[message] Data received from server:    ${event.data}`);
-//     };
+    //on update from server
+    placeteam.websocket.onmessage = function(event) {
+        if(event.data.command == 'update'){
+            placeteam.update(event.data);
+        }
+        else if(event.data.command == 'cooldown'){
+            //show visuals with cooldown
+        }
+    };
 
-//     //send update to server
-//     placeteam.websocket.send()
-//     //closing connection
-//     placeteam.websocket.onclose = function(event) {
-//     if (event.wasClean) {
-//         console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-//     } else {
-//         // e.g. server process killed or network down
-//         // event.code is usually 1006 in this case
-//         console.log('[close] Connection died');
-//     }
-//     };
-//     //
-//     placeteam.websocket.onerror = function(error) {
-//         console.log(`[error]`,error);
-//     };
-// });
+    //send update to server
+    placeteam.websocket.send()
+    //closing connection
+    placeteam.websocket.onclose = function(event) {
+    if (event.wasClean) {
+        console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        console.log('[close] Connection died');
+    }
+    };
+    //
+    placeteam.websocket.onerror = function(error) {
+        console.log(`[error]`,error);
+    };
+});
