7a7e2075c34290f37fd82090ec1a45ee226487c2
MariusKachelLEBK <123642091+MariusKachelLEBK@users.noreply.github.com>
2023-02-16 12:26:15 +0000

    Update center zoom

--- scripts/main.js
+++ scripts/main.js
@@ -94,10 +94,15 @@ window.addEventListener("load", (event) => {
             placeteam.mapcontainer.style.cssText = "overflow: scroll;";
         }
         initialWidth = placeteam.canvas.clientWidth;
+        initialHeight = placeteam.canvas.clientHeighth;
+
         placeteam.canvas.style.cssText = 'width: ' + currentCanvasWidth + '%;';
+
         newWidth = placeteam.canvas.clientWidth;
+        newHeight = placeteam.canvas.clientHeighth;
         halfWidthDifference = (newWidth - initialWidth) / 2;
-        placeteam.mapcontainer.scrollBy(halfWidthDifference, halfWidthDifference);
+        halfHeightDifference = (newHeight - initialHeight) / 2;
+        placeteam.mapcontainer.scrollBy(halfWidthDifference, halfHeightDifference);
     });
 
     placeteam.mapcontainer.addEventListener('wheel', function(event) {
