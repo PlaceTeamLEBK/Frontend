f536c72df107e88541ad65bae4862e6ab9e4a2ec
MariusKachelLEBK <123642091+MariusKachelLEBK@users.noreply.github.com>
2023-03-09 11:49:34 +0000

    Update MouseState

--- scripts/modules/navigation.mjs
+++ scripts/modules/navigation.mjs
@@ -11,17 +11,17 @@ export class Navigation {
     }
 
     Pan() {
-        placeteam.canvas.addEventListener("mousemove", function(event) {
+        this.placeteam.canvas.addEventListener("mousemove", function(event) {
             if (mouseMovement.mouseIsDown) {
                 const offsetX = event.movementX * -1;
                 const offsetY = event.movementY * -1;
     
-                placeteam.mapcontainer.scrollBy(offsetX, offsetY);
+                this.placeteam.mapcontainer.scrollBy(offsetX, offsetY);
             }
             if(mouseMovement.rightclickIsDown){
-                const mouseCoordinates = placeteam.getCoordinateslAtMouse(event);
-                const rgbArray = placeteam.ctx.getImageData(mouseCoordinates.x, mouseCoordinates.y, 1, 1).data; 
-                placeteam.changeColor("#"+placeteam.rgbToHex(rgbArray[0],rgbArray[1],rgbArray[2]),placeteam.colorcontainer.querySelector('.select .selected').dataset.colorid);
+                const mouseCoordinates = this.placeteam.getCoordinateslAtMouse(event);
+                const rgbArray = this.placeteam.ctx.getImageData(mouseCoordinates.x, mouseCoordinates.y, 1, 1).data; 
+                this.placeteam.changeColor("#"+this.placeteam.rgbToHex(rgbArray[0],rgbArray[1],rgbArray[2]),this.placeteam.colorcontainer.querySelector('.select .selected').dataset.colorid);
             }
         });
     }
