c17b4e4f92422c80b2cd718ec5b57ea28ceb62ad
MariusKachelLEBK <123642091+MariusKachelLEBK@users.noreply.github.com>
2023-03-16 09:31:30 +0000

    Improve ZoomSlider logic

--- scripts/modules/ZoomSlider.mjs
+++ scripts/modules/ZoomSlider.mjs
@@ -3,6 +3,11 @@ export class ZoomSlider {
     navigation = null;
     canvasManipulator = null;
 
+    zoomOptions = {
+        In: "In",
+        Out: "Out"
+    };
+
     constructor(placeteam, navigation, canvasManipulator) {
         this.placeteam = placeteam;
         this.navigation = navigation;
@@ -21,33 +26,34 @@ export class ZoomSlider {
     SetZoomInEvent() {
         let _self = this;
         document.getElementById("btn_zoom_plus").addEventListener('click', function(event){
-            let minZoom = _self.placeteam.minZoomPercentageMobile;
-            if (_self.placeteam.desktopMediaQuery.matches) {
-                minZoom = _self.placeteam.minZoomPercentageDesktop;
-            } else if (_self.placeteam.tabletMediaQuery.matches) {
-                minZoom = _self.placeteam.minZoomPercentageTablet;
-            }
-            let newCanvasWidth = _self.canvasManipulator.GetCanvasWidthPercentageInt() * _self.placeteam.zoomSpeed;
-            let normalizedCanvasWidth = Math.max(minZoom, newCanvasWidth);
-            normalizedCanvasWidth = Math.min(_self.placeteam.maxZoom, normalizedCanvasWidth);
-            _self.navigation.SetZoom(normalizedCanvasWidth);
+            _self.Zoom(_self.zoomOptions.In);
         });
     }
 
     SetZoomOutEvent() {
         let _self = this;
         document.getElementById("btn_zoom_minus").addEventListener('click', function(event){
-            let minZoom = _self.placeteam.minZoomPercentageMobile;
-            if (_self.placeteam.desktopMediaQuery.matches) {
-                minZoom = _self.placeteam.minZoomPercentageDesktop;
-            } else if (_self.placeteam.tabletMediaQuery.matches) {
-                minZoom = _self.placeteam.minZoomPercentageTablet;
-            }
-            let newCanvasWidth = _self.canvasManipulator.GetCanvasWidthPercentageInt() / _self.placeteam.zoomSpeed;
-            let normalizedCanvasWidth = Math.max(minZoom, newCanvasWidth);
-            normalizedCanvasWidth = Math.min(_self.placeteam.maxZoom, normalizedCanvasWidth);
-            _self.navigation.SetZoom(normalizedCanvasWidth);
+            _self.Zoom(_self.zoomOptions.Out);
         });
+    }
+
+    Zoom(zoomOption) {
+        let minZoom = this.placeteam.minZoomPercentageMobile;
+        if (this.placeteam.desktopMediaQuery.matches) {
+            minZoom = this.placeteam.minZoomPercentageDesktop;
+        } else if (this.placeteam.tabletMediaQuery.matches) {
+            minZoom = this.placeteam.minZoomPercentageTablet;
+        }
+        let newCanvasWidth;
+        switch(zoomOption) {
+            case this.zoomOptions.In:
+                newCanvasWidth = this.canvasManipulator.GetCanvasWidthPercentageInt() * this.placeteam.zoomSpeed;
+            case this.zoomOptions.Out:
+                newCanvasWidth = _self.canvasManipulator.GetCanvasWidthPercentageInt() / this.placeteam.zoomSpeed;
+        }
+        let normalizedCanvasWidth = Math.max(minZoom, newCanvasWidth);
+        normalizedCanvasWidth = Math.min(this.placeteam.maxZoom, normalizedCanvasWidth);
+        this.navigation.SetZoom(normalizedCanvasWidth);
     }
 
     SetSliderEvent() {
